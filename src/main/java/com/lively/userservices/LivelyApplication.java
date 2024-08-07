849dafacf49abf23b492ce6fa11928fc35dbbe7b
Revanth Madasu <revanthmadasu913@gmail.com>
2024-08-07 18:11:52 -0500

    Project creation

---
+++ src/main/java/com/lively/userservices/LivelyApplication.java
@@ -0,0 +1,13 @@
+package com.lively.userservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivelyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lively/userservices/LivelyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lively.userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivelyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
